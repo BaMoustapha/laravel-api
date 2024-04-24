64092adb3455f098887f8ef0b14aafd7b53b20b3
Santos25 <abdoulayejuniorsantos@gmail.com>
2024-04-24 18:04:56 +0200

    affichage des user et des shop

--- app/Filament/Resources/ShopResource.php
+++ app/Filament/Resources/ShopResource.php
@@ -10,6 +10,7 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Filament\Tables\Columns;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
@@ -23,15 +24,44 @@ class ShopResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Forms\Components\Group::make()
+                ->schema([
+                    Forms\Components\Group::make()
+                    
+                    ->schema([ // Utilisation de Group pour créer une section
+                        Forms\Components\TextInput::make('name'),
+                        Forms\Components\TextInput::make('logo'),
+                        Forms\Components\TextInput::make('telephone')
+                        
+                        ->columnSpan( span : 'full')
+                        ])->columns(2),
+                    ]),
+                    Forms\Components\Group::make()
+                    
+                    ->schema([ // Utilisation de Group pour créer une section
+                        Forms\Components\TextInput::make('email'),
+                        Forms\Components\TextInput::make('adresse'),
+                        Forms\Components\MarkdownEditor::make('description')
+                        
+
+                            ->columnSpan( span : 'full')
+                    ])->columns(2),
             ]);
     }
 
     public static function table(Table $table): Table
     {
+
+        $shops = Shop::all();
+
+
         return $table
             ->columns([
-                //
+                Columns\ImageColumn::make(name:'Image'),
+                Columns\TextColumn::make(name:'name'),
+                Columns\TextColumn::make(name:'description'),
+                Columns\TextColumn::make(name:'email'),
+                Columns\TextColumn::make(name:'telephone')
             ])
             ->filters([
                 //
