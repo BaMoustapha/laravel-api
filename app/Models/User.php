377fbed4e0182030d6e380fa90ccb075577d72fe
Mbaye Ndiaye <babacarndiay546@gmail.com>
2024-05-15 17:04:53 +0000

    gestion

--- app/Models/User.php
+++ app/Models/User.php
@@ -73,7 +73,7 @@ class User extends Authenticatable implements JWTSubject
 
 
 
-    public function shops()
+    public function shop()
     {
         return $this->hasOne(Shop::class, 'user_id');
     }
